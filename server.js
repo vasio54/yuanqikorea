710aee72a4c5631b769f9f99f12028db9e698bc5
Glitch (glitch-hello-node) <none>
2025-03-21 04:18:16 +0000

    🙈😂 Checkpoint ./server.js:76559215/7803 ./package.json:76559215/2003 ./index.html:76559215/2550 ./chat.html:76559215/7882

--- server.js
+++ server.js
@@ -1,119 +1,139 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const path = require("path");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
+app.use(express.static(__dirname));
+
+let waitingMales = [];
+let waitingFemales = [];
+let roomCounter = 0;
+
+io.on('connection', (socket) => {
+    console.log('有用戶連線！', socket.id);
+
+    socket.on('startMatching', (data) => {
+        console.log('收到 startMatching 事件，data:', data);
+        if (!data || !data.targetGender) {
+            socket.emit('error', '請先選擇聊天對象性別！');
+            socket.disconnect();
+            return;
+        }
+
+        const { targetGender } = data;
+        const userGender = targetGender === 'male' ? 'female' : 'male';
+        socket.gender = userGender;
+        console.log('用戶性別:', userGender, 'socket.id:', socket.id);
+
+        if (userGender === 'male') {
+            console.log('等待中的女性數量:', waitingFemales.length);
+            if (waitingFemales.length > 0) {
+                const partner = waitingFemales.shift();
+                const room = `room-${roomCounter++}`;
+                console.log('配對成功，房間:', room);
+
+                socket.join(room);
+                partner.join(room);
+                socket.currentRoom = room;
+                partner.currentRoom = room;
+
+                socket.emit('matchSuccess', { room });
+                partner.emit('matchSuccess', { room });
+            } else {
+                waitingMales.push(socket);
+                socket.emit('waiting', `正在尋找女性聊天對象...`);
+            }
+        } else if (userGender === 'female') {
+            console.log('等待中的男性數量:', waitingMales.length);
+            if (waitingMales.length > 0) {
+                const partner = waitingMales.shift();
+                const room = `room-${roomCounter++}`;
+                console.log('配對成功，房間:', room);
+
+                socket.join(room);
+                partner.join(room);
+                socket.currentRoom = room;
+                partner.currentRoom = room;
+
+                socket.emit('matchSuccess', { room });
+                partner.emit('matchSuccess', { room });
+            } else {
+                waitingFemales.push(socket);
+                socket.emit('waiting', `正在尋找男性聊天對象...`);
+            }
+        }
+    });
+
+    socket.on('message', (msg) => {
+        console.log('收到訊息:', msg, 'socket.id:', socket.id);
+        const rooms = Array.from(socket.rooms);
+        console.log('用戶所在房間:', rooms);
+        if (rooms.length > 1) {
+            const room = rooms[1];
+            console.log('轉發訊息到房間:', room);
+            io.to(room).emit('message', { user: socket.id, text: msg });
+        } else {
+            console.log('用戶不在任何聊天室中，無法轉發訊息');
+            socket.emit('error', '聊天室已失效，請重新配對！');
+        }
+    });
+
+    socket.on('typing', () => {
+        const room = socket.currentRoom;
+        if (room) {
+            socket.to(room).emit('typing');
+        }
+    });
+
+    socket.on('stopTyping', () => {
+        const room = socket.currentRoom;
+        if (room) {
+            socket.to(room).emit('stopTyping');
+        }
+    });
+
+    socket.on('joinRoom', (room) => {
+        console.log('用戶重新加入房間:', room, 'socket.id:', socket.id);
+        socket.join(room);
+        socket.currentRoom = room;
+    });
+
+    socket.on('leaveChat', () => {
+        console.log('收到 leaveChat 事件，socket.id:', socket.id);
+        const room = socket.currentRoom;
+        if (room) {
+            socket.to(room).emit('partnerLeft', '對方已離開，你將被導回首頁...');
+            socket.leave(room);
+            socket.currentRoom = null;
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('用戶斷線，socket.id:', socket.id);
+        const room = socket.currentRoom;
+        if (room) {
+            socket.to(room).emit('partnerLeft', '對方已斷線，你將被導回首頁...');
+        }
+
+        if (socket.gender === 'male') {
+            const index = waitingMales.indexOf(socket);
+            if (index !== -1) {
+                waitingMales.splice(index, 1);
+            }
+        } else if (socket.gender === 'female') {
+            const index = waitingFemales.indexOf(socket);
+            if (index !== -1) {
+                waitingFemales.splice(index, 1);
+            }
+        }
+        console.log('用戶已斷開連線', socket.id);
+    });
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`伺服器運行於 http://localhost:${PORT}`);
 });
-
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
